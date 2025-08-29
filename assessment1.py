878ac3a56077d0cc46b593cd2b488887e4839ffd
Jonny Huck <jonathan.huck@manchester.ac.uk>
2025-08-29 15:43:26 +0100

    add Python template

---
+++ assessment1.py
@@ -0,0 +1,19 @@
+"""
+Understanding GIS: Assessment 1
+@author [INSERT STUDENT NUMBER HERE]
+"""
+from time import perf_counter
+
+# set start time
+start_time = perf_counter()	
+
+# --- NO CODE ABOVE HERE ---
+
+
+''' --- ALL CODE MUST BE INSIDE HERE --- '''
+
+
+# --- NO CODE BELOW HERE ---
+
+# report runtime
+print(f"completed in: {perf_counter() - start_time} seconds")
